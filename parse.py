77251bffed5a7ad43e61ff2790c85468244a4f7c
MG4CE <maged.othm@gmail.com>
2020-12-25 19:42:06 -0500

    renamed parser.py to parse.py due to importing conflict

--- parse.py
+++ parse.py
@@ -52,19 +52,55 @@ def clean_list(line_list):
     Returns:
         list: newly updated list of string.
     """
-    line_list = [line for line in line_list if line is not "\n" and not line.strip().startswith("#", 0)]
+    line_list = [line for line in line_list if line != "\n" and not line.strip().startswith("#", 0)]
     line_list = [line.replace("\n", "") for line in line_list]
     return line_list
 
-def extract_email_input(line_list):
-    return EMAIL_PARAMS
+def extract_email_input(filename):
+    file = read_file(filename)
+    line_list = convert_file_to_list(file)
+
+    email_inputs = []
+
+    for line in line_list:
+        split_line = line.split('=', 1)
+
+        if  split_line[0] not in EMAIL_PARAMS:
+            raise Exception("Entry " + split_line[0] + " in " + filename + " is unknown! only " + EMAIL_PARAMS + " are allowed!")
+        else:
+            if split_line[1].strip() == "\n" or split_line[1].strip() == "":
+                raise Exception("Entry " + split_line[0] + " in " + filename + " is empty!")
+            email_inputs.append(split_line[1].replace("\n", ""))
+
+    return email_inputs
 
 def extract_track_inputs(line_list):
-    return TRACK_PARAMS
+    list_inputs = []
+    track_entry = []
+    counter = 0
+
+    for line in line_list:
+        entry = line.split('=', 1)
+        if entry[0] == TRACK_PARAMS[counter % len(TRACK_PARAMS)]:
+            track_entry.append(entry[1])
+            counter = counter + 1
+            if counter % len(TRACK_PARAMS) == 0:
+                list_inputs.append(track_entry)
+                track_entry = []
+        else:
+            raise Exception("Unexpected track entry, make sure track entries follow template!")
+
+    if counter % len(TRACK_PARAMS) != 0:
+        raise Exception("edge case!")
+
+    return list_inputs 
 
 def process_track_input(track_list):
     return []
 
 if __name__ == "__main__":
     file_list = convert_file_to_list(read_file("track.txt"))
-    print(clean_list(file_list))
+    file_list = clean_list(file_list)
+    for x in extract_track_inputs(file_list):
+        print(x)
+    print(extract_email_input("config.cfg"))
